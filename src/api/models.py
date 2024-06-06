8dad796616522f1970e1c90ee62f0ea2812f4603
Marina <150665821+Marinamb19@users.noreply.github.com>
2024-06-06 19:01:42 +0000

    jwt marinamb

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    logout_activities = db.relationship('LogoutActivity', backref='user', lazy=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.username}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username     
         }
+
+class LogoutActivity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    logout_time = db.Column(db.DateTime, nullable=False)
+
+class RevokeToken(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(120), unique=True, nullable=False)
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,22 +1,90 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from flask_migrate import Migrate
 from flask_cors import CORS
+from api.utils import APIException
+from api.admin import setup_admin
+from api.models import db, User, LogoutActivity, RevokeToken
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager, get_jwt
+from datetime import datetime
+
 
 api = Blueprint('api', __name__)
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-# Allow CORS requests to this API
-CORS(api)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "super-secret" 
+
+CORS(app)
+Migrate(app, db)
+setup_admin(app)
+jwt = JWTManager(app)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+
+    if User.query.filter_by(username=data['username']).first():
+        return jsonify({"message": "User already exists"}), 400
+    
+    new_user = User(
+        email=data['email'],
+        username=data['username'],
+        password=data['password'],
+        is_active=True  
+    )    
+    db.session.add(new_user)
+    db.session.commit()
+    serialized_new_user = new_user.serialize()
+
+    return jsonify(serialized_new_user), 200
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    
+    if not user.check_password(password):
+        return jsonify({"message": "Invalid password"}), 401
+
+    
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token), 200
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+# //TODO logout 
+@app.route("/logout", methods=["POST"])
+@jwt_required()
+def logout():
+    jwt_token = get_jwt()
+
+    
+    current_user = get_jwt_identity()
+    logout_time = datetime.now(datetime.UTC)
+    logout_activity = LogoutActivity(user_id=current_user.id, logout_time=logout_time)
+    db.session.add(logout_activity)
+    db.session.commit()
+
+    revoked_token = RevokeToken(jti=jwt_token["jti"])
+    db.session.add(revoked_token)
+    db.session.commit()
+
+    return jsonify({"message": "Logout successful"}), 200
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+if __name__ == "__main__":
+    app.run()
